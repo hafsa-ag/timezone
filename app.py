edad591dc8be6105f80f302e37758adb0363b21d
Hafsa Agnaou <5425575-Agnaou@users.noreply.gitlab.com>
2020-08-04 22:34:24 +0100

    flask cors

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 """Main flask app for parsing restitutions xls"""
 
 from flask import Flask, jsonify, request
+frm flask_cors import CORS, cross_origin
 from modules.scrapingCacaoData import scrapCacaoToJson
 import sys
 import logging
 
 app = Flask(__name__)
 # heroku logging
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
 
